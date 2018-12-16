80a176d37629e60c5094e9e3dfdd37ae85f4fa7b
Elias Ventura <eliasventura23@gmail.com>
2018-12-16 00:35:32 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE projects (
+  id PRIMARY KEY INTEGER,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date INTEGER,
+  end_date INTEGER
+);
+
+CREATE TABLE users (
+  id PRIMARY KEY INTEGER,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  id PRIMARY KEY INTEGER,
+  project_id INTEGER,
+  user_id INTEGER
+);
